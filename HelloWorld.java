cbf9aaecd37b4c570c9135b5cb65a6d0b036abdd
Admin <finvvs@ukr.net>
2022-02-01 15:32:40 +0200

    implement SUB function

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,11 +2,14 @@ public class HelloWorld {
     public static void main(String[] args) {
         int a = 5;
         int b = 3;
-
+        System.out.println( sub (a, b) + "  " + "please enter Hello World");
         System.out.println( add (a, b) + "  " + "please enter Hello World");
     }
     public static int add (int a, int b){
         return a + b;
+    }
+    public static int sub (int a, int b){
+        return a - b;
     }
 
 }
