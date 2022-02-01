bb6f17b03769e01aba400107410e02b41a24b8d9
Admin <finvvs@ukr.net>
2022-02-01 15:35:39 +0200

    implement del function

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
         System.out.println( sub (a, b) + "  " + "please enter Hello World");
         System.out.println( add (a, b) + "  " + "please enter Hello World");
         System.out.println( mul (a, b) + "  " + "please enter Hello World");
+        System.out.println( del (a, b) + "  " + "please enter Hello World");
     }
     public static int add (int a, int b){
         return a + b;
@@ -15,6 +16,8 @@ public class HelloWorld {
     public static int mul (int a, int b){
         return a * b;
     }
-
+    public static int del (int a, int b){
+        return a / b;
+    }
 
 }
